fa24c9cec921032d3eca518fab22951a89d959fb
Natsu <andrewtt824@gmail.com>
2026-08-12 09:45:55 -0500

    redis cache en categorias

--- app/routers/categorias.py
+++ app/routers/categorias.py
@@ -32,21 +32,30 @@ def get_categorias(db = Depends(database.get_db_postgresql))-> list[Categoria]:
 
 @router.get("/categorias/{id}")
 def get_categoria(id: int, db = Depends(database.get_db_postgresql))-> Categoria:
+    cache = redis_client.get(f'categoria_{id}')
+    if cache is not None:
+        categoria = json.loads(cache)
+        print("Entro en cache")
+        return Categoria(**categoria)
+
     cursor = db.cursor()
     cursor.execute("SELECT * FROM categorias WHERE id = %s", (id,))
     resultado = cursor.fetchone()
     if resultado is None:
         raise HTTPException(status_code=404, detail="Categoria no existe")
-
-    return Categoria(id=id, titulo_categoria=resultado["titulo_categoria"])
+    categoria = Categoria(id=id, titulo_categoria=resultado["titulo_categoria"])
+    redis_client.set(f'categoria_{id}', json.dumps(categoria.model_dump()) , ex= CACHE_TTL)
+    return categoria
 
 @router.post("/categorias")
 def crear_categoria(categoria: CrearCategoria, db=Depends(database.get_db_postgresql))-> Categoria:
     cursor = db.cursor()
     cursor.execute("INSERT INTO categorias (titulo_categoria) VALUES (%s) RETURNING id", (categoria.titulo_categoria,))
     fetch_ans = cursor.fetchone()
-
     db.commit()
+
+    redis_client.delete('categorias')
+
     return Categoria(id=fetch_ans["id"], titulo_categoria=categoria.titulo_categoria)
 @router.patch("/categorias/{id}")
 def actualizar_categoria(id: int, actualizarCategoria: ActualizarCategoria, db=Depends(database.get_db_postgresql))-> Categoria:
@@ -58,9 +67,12 @@ def actualizar_categoria(id: int, actualizarCategoria: ActualizarCategoria, db=D
 
     cursor.execute("UPDATE categorias SET titulo_categoria = %s WHERE id = %s", (actualizarCategoria.titulo_categoria, id))
     db.commit()
+
+    redis_client.delete('categorias')
+    redis_client.delete(f'categoria_{id}')
     return Categoria(id=id, titulo_categoria=actualizarCategoria.titulo_categoria)
 
-@router.delete("/categorias{id}")
+@router.delete("/categorias/{id}")
 def eliminar_categoria(id: int, db=Depends(database.get_db_postgresql))-> Categoria:
     cursor = db.cursor()
     cursor.execute("SELECT * FROM categorias WHERE id = %s ", (id,))
@@ -70,6 +82,7 @@ def eliminar_categoria(id: int, db=Depends(database.get_db_postgresql))-> Catego
 
     cursor.execute("DELETE FROM categorias WHERE id = %s", (id,))
     db.commit()
-
+    redis_client.delete('categorias')
+    redis_client.delete(f'categoria_{id}')
     return Categoria(id=id, titulo_categoria=resultado["titulo_categoria"])
 
